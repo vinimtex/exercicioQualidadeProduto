73d5ec5a5a8ff4e59f1132296a7d82dafa6dd64f
Laura <37625323+laurascclaro@users.noreply.github.com>
2019-09-16 21:44:07 -0300

    Create Node.java

---
+++ src/main/java/pucrs/qp/exercicio/Node.java
@@ -0,0 +1,24 @@
+public class Node {
+	private String data;
+	private Node next;
+	public String getData();
+	return data;
+}
+
+public void setData (String data){
+	this.data = this.data;
+}
+public Node getNext(){
+	return next;
+}
+public void setNext (Node next){
+	this.next = next;
+}
+public Node (String data, Node next){
+	this.data = this.data;
+	next = next;
+}
+public String toString (){
+	return data;
+}
+
