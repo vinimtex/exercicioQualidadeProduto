890ac20f45dc53ffe61178fbee54b40211500c5b
Vinícius Teixeira <vinicius.teixeira@somosphi.com>
2019-09-16 21:48:10 -0300

    fix

--- src/main/java/pucrs/qp/exercicio/LinkedList.java
+++ src/main/java/pucrs/qp/exercicio/LinkedList.java
@@ -1,35 +1,37 @@
+package pucrs.qp.exercicio;
 
 public class LinkedList {
 	Node head;
 	Node tail;
-	
-	public void addNodeInLast (Node node){
+
+	public void addNodeInLast(Node node) {
 		if (null != tail) {
-			head=node;
+			head = node;
 			tail = node;
 		} else {
 			tail.setNext(node);
-			tail=node;
+			tail = node;
 		}
 	}
-	public int getSize (){
+
+	public int getSize() {
 		return traverse();
 	}
-	public int traverse (){
-	int listSize = 0;
-	if(head==null) {
-	return listSize;
-	}
-	else{
-	Node tempHead = head;
-	while (tempHead != null) {
-	System.out.print ("Node: " + tempHead.toString());
-	System.out.println (", ");
-	tempHead = tempHead.getNext();
-	listSize++;
-	}
-	System.out.println ("");
-	}
-	return listSize;
+
+	public int traverse() {
+		int listSize = 0;
+		if (head == null) {
+			return listSize;
+		} else {
+			Node tempHead = head;
+			while (tempHead != null) {
+				System.out.print("Node: " + tempHead.toString());
+				System.out.println(", ");
+				tempHead = tempHead.getNext();
+				listSize++;
+			}
+			System.out.println("");
+		}
+		return listSize;
 	}
 }
